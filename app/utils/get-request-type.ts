ebeded2a1cd0bb1e55a5713eebc3bae8bfed7311
Michal Vrbata <vrbata@outlook.com>
2025-04-18 18:52:10 +0200

    Remove utils

--- app/utils/get-request-type.ts
+++
@@ -1,7 +0,0 @@
-export function getRequestType(request: Request) {
-  if (request.headers.get("Sec-Fetch-Dest") === "document") {
-    return "document" as const;
-  }
-
-  return "fetch" as const;
-}
