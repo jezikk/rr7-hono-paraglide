72d6e8025ae945cdf27730b7a80e3a5f2551d5b8
Michal Vrbata <vrbata@outlook.com>
2025-05-17 20:08:46 +0200

    Remove hono

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,9 @@
 import type { Config } from "@react-router/dev/config";
 
-declare module "react-router" {
-  interface Future {
-    unstable_middleware: true;
-  }
-}
-
 export default {
   future: {
     unstable_splitRouteModules: true,
+    unstable_viteEnvironmentApi: true,
     unstable_middleware: true,
   },
   ssr: true,
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouterDevTools } from "react-router-devtools";
-import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    reactRouterHonoServer(),
     reactRouterDevTools(),
     reactRouter(),
     tsconfigPaths(),
